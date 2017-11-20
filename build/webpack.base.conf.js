446883f989702ea350c9e1dede266aee7b7975f4
ChenJing <502269499@qq.com>
2017-11-20 18:02:11 +0800

    图片、css、字体等文件处理，可作为示例

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,5 +6,17 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.join(__dirname, '../dist'),
 		publicPath: '/'
+	},
+	module: {
+		rules: [{
+			test: /.css$/,
+			use: ['style-loader', 'css-loader']
+		}, {
+			test: /\.(png|jpg|jpeg|svg|gif|bmp|ico)$/,
+			use: ['file-loader']
+		}, {
+			test: /\.(woff|woff2|eot|ttf|otf)$/,
+			use: ['file-loader']
+		}]
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import './index.css'
+import icon from './img.jpg'
+import './icomoon/style.css'
+
 const div = document.createElement('div')
 div.innerText = 'Add child element.'
 document.body.appendChild(div)
+
+const img = new Image()
+img.src = icon
+document.body.appendChild(img)
+
+const span = document.createElement('span')
+span.className = 'icon-home'
+document.body.appendChild(span)
