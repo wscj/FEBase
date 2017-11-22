1f82b1e9a6321e22031b3d840389bbad9b743c6b
ChenJing <502269499@qq.com>
2017-11-22 12:03:29 +0800

    添加url-loader把小图片转换为base64

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -13,9 +13,6 @@ module.exports = {
 			test: /.css$/,
 			use: ['style-loader', 'css-loader']
 		}, {
-			test: /\.(png|jpg|jpeg|svg|gif|bmp|ico)$/,
-			use: ['file-loader']
-		}, {
 			test: /\.(woff|woff2|eot|ttf|otf)$/,
 			use: ['file-loader']
 		}, {
@@ -24,6 +21,15 @@ module.exports = {
 		}, {
 			test: /\.less$/,
 			use: ['style-loader', 'css-loader', 'less-loader']
+		}, {
+		// 	test: /\.(png|jpg|jpeg|svg|gif|bmp|ico)$/,
+		// 	use: ['file-loader']
+		// }, {
+	        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+	        loader: 'url-loader',
+	        options: {
+	          limit: 10000
+	        }
 		}]
 	},
 	plugins: [
